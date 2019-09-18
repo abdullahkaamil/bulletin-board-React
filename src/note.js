7d691e815a0bd29d6c5566fd5429a3799bbc890a
digitaltradeYazilim <yazilim@digitaltrade.com.tr>
2019-09-18 10:42:59 +0200

    updating the cards

--- src/note.js
+++ src/note.js
@@ -3,62 +3,67 @@ import { FaPen } from 'react-icons/fa'
 import { FaTrash } from 'react-icons/fa'
 import { FaSave } from 'react-icons/fa'
 
-
 class Note extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            editing: false
-        }
+	constructor(props) {
+		super(props)
+		this.state = {
+			editing: false
+		}
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+		this.save = this.save.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
+	}
+	edit() {
+		this.setState({
+			editing: true
+		})
+	}
 
-        this.edit = this.edit.bind(this)
-        this.remove = this.remove.bind(this)
-        this.save = this.save.bind(this)
-        this.renderForm = this.renderForm.bind(this)
-        this.renderDisplay = this.renderDisplay.bind(this)
+	remove() {
+		alert('removing note')
+	}
 
-    }
+	save(e) {
+		e.preventDefault()
+		this.props.onChange(this._newText.value, this.props.index)
+		this.setState({
+			editing: false
+		})
+	}
 
-    edit(){
-        this.setState({
-            editing: true
-        })
-    }
+	renderForm() {
+		return (
+			<div className="note">
+				<form onSubmit={this.save}>
+					<textarea ref={input => this._newText = input}/>
+					<button id="save"><FaSave /></button>
+				</form>
+			</div>
+		)
+	}
 
-    remove(){
-        alert('removing note')
-    }
-    
-    save(){
-        alert(this._newText.value)
-    }
-    
-    renderForm(){
-        return(
-            <div className="note">
-                <form>
-                    <textarea ref={input => this._newText = input } />    
-                    <button onClick={this.save}> <FaSave/> </button>
-                </form>
-            </div>
-        )
-    }
+	renderDisplay() {
+		return (
+			<div className="note">
+				<p>{this.props.children}</p>
+				<span>
+					<button onClick={this.edit} id="edit"><FaPen /></button>
+					<button onClick={this.remove} id="remove"><FaTrash /></button>
+				</span>
+			</div>
+		)
+	}
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay()
+	}
 
-    renderDisplay() {
-        return (
-            <div className="note">
-                <p>{this.props.children}</p>
-                <span>
-                    <button onClick={this.edit} id='edit'><FaPen  /></button>
-                    <button onClick={this.remove} id='remove'><FaTrash  /></button>
-                </span>
-            </div>
-        )
-    }
-
-    render(){
-        return this.state.editing ? this.renderForm() : this.renderDisplay()
-    }
 }
 
 export default Note
+
+
+
+
+
